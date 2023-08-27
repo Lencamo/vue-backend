b268eca32f09364d060f6b687c2ff48f0846d409
Lencamo <207141481@qq.com>
2023-08-27 20:50:31 +0800

    /user/login和/user/info/all接口改用mock server

--- mock/api-mock/user.js
+++ mock/api-mock/user.js
@@ -1,6 +1,84 @@
 const Mock = require('mockjs')
 
+const TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.eyJ1dWlkIjoiOGZlNGUyMzAxMDIxNDMzYThlMTk1M2RhOTIwMWMyNmQiLCJpYXQiOjE2NjUxMzM1MjcsImV4cCI6MTY2NjM0MzEyN30.XlapA40qzy1czIXzRjBKdPXrW8pn-OzDeiu_3HxQYDg'
+
+const { userInfo } = Mock.mock({
+  userInfo: {
+    role: ['user'],
+    name: '@cname()',
+    classes: '软件@string(number,1,2).@string(number,1,2)本',
+    studentId: '@id()',
+    email: '',
+    uuid: '@guid()'
+  }
+})
+
 module.exports = [
   // 用户登录接口 ✔
+  {
+    url: '/user/login',
+    type: 'post',
+    response: (config) => {
+      // console.log(config.body)
+      const { email, password } = config.body
+
+      userInfo.email = email
+
+      // console.log(userInfo)
+      switch (email) {
+        case '11111111@qq.com':
+          userInfo.role[0] = 'super-admin'
+          break
+        case '22222222@qq.com':
+          userInfo.role[0] = 'root'
+          break
+        case '33333333@qq.com':
+          userInfo.role[0] = 'oj-admin'
+          break
+        case '44444444@qq.com':
+          userInfo.role[0] = 'teacher'
+          break
+        default:
+          userInfo.role[0] = 'user'
+          break
+      }
+
+      // console.log(userInfo)
+      return {
+        code: 200,
+        msg: '登录成功',
+        data: {
+          message: 'success',
+          token: TOKEN
+        }
+      }
+    }
+  },
+
   // 根据token获取用户信息接口 ✔
+  {
+    url: '/user/info/all',
+    type: 'post',
+    response: (config) => {
+      const { tokenValue } = config.body
+      console.log(tokenValue)
+
+      if (tokenValue === TOKEN) {
+        return {
+          code: 200,
+          msg: '查询成功！',
+          data: userInfo
+        }
+      } else {
+        return {
+          code: 500,
+          data: {
+            message: 'Not Authenticated'
+          },
+          msg: '用户认证错误！'
+        }
+      }
+    }
+  }
 ]
--- src/api/user.js
+++ src/api/user.js
@@ -7,10 +7,14 @@ import request from '@/utils/request'
  */
 export function loginAPI(data) {
   return request({
-    url: 'http://127.0.0.1:4523/m1/1713872-0-default/user/login',
+    // url: 'http://127.0.0.1:4523/m1/1713872-0-default/user/login',
     // url: 'http://10.22.130.17:30892/user/login',
     // url: 'http://10.22.130.17/api/user/login',
     // url: 'http://eaip.com/api/user/login',
+    // method: 'post',
+    // data
+
+    url: '/user/login',
     method: 'post',
     data
   })
@@ -25,11 +29,15 @@ export function getUserInfoAllAPI(tokenValue) {
   // const url = '/user/info/all/token/' +
   // console.log(url)
   return request({
-    url: `http://127.0.0.1:4523/m1/1713872-0-default/user/info/all/token/${tokenValue}`,
+    // url: `http://127.0.0.1:4523/m1/1713872-0-default/user/info/all/token/${tokenValue}`,
     // url: `http://10.22.130.17:30892/user/info/all/token/${tokenValue}`,
     // url: `http://10.22.130.17/api/user/info/all/token/${tokenValue}`,
     // url: `http://eaip.com/api/user/info/all/token/${tokenValue}`,
     // url: 'http://eaip.com/api/user/info/all/token/' + tokenValue,
-    method: 'get'
+    // method: 'get'
+
+    url: '/user/info/all',
+    method: 'post',
+    data: { tokenValue }
   })
 }
